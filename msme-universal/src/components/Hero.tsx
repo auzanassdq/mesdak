193ef777dad33faef74b7e293df21f0168c4fa57
auzanassdq <auzanassidqi@gmail.com>
2026-07-06 10:10:46 +0700

    fix: scroll behave

--- msme-universal/src/components/Hero.tsx
+++ msme-universal/src/components/Hero.tsx
@@ -172,7 +172,7 @@ const Hero = () => {
 
         <div className="hero-description">
           <p className="text-xl md:text-xl lg:text-2xl mb-12 text-gray-200 max-w-3xl mx-auto leading-normal">
-            Digital Infrastructures & Solutions to power MSMEs,
+            Economic Digital Infrastructure & Solutions to power MSMEs,
             <br /> Financial Institutions and Governments
           </p>
         </div>
